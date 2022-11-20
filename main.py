e8d5b3ed2472f6a94b2355f0e8690b6fcceb77a0
GCipry3 <galbeazaciprian30@gmail.com>
2022-11-20 20:49:47 +0200

    added start page and endgame page

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ ASSETS = "assets/"
 
 clock = pygame.time.Clock()
 
+SCORE_FONT = pygame.font.SysFont("comicsans", 80)
+END_FONT = pygame.font.SysFont("comicsans", 60)
+
 
 class Bird:
     def __init__(self):
@@ -61,6 +64,8 @@ class Pipe:
         self.top_y = self.height - self.TOP_PIPE.get_height()
         self.bottom_y = self.height + gap
 
+        self.passed_flag = False
+
     def move(self):
         self.x -= self.vel
 
@@ -86,7 +91,8 @@ class FlappyBirdGame:
         self.bird = Bird()
         self.bg_img = pygame.image.load(ASSETS + "background.jpeg")
         self.keyboard_timer = 0
-        self.pipes = [Pipe(400)]
+        self.pipes = [Pipe(WIDTH)]
+        self.removed_pipes = []
 
     def draw(self):
         window.blit(self.bg_img, (0, 0))
@@ -94,30 +100,104 @@ class FlappyBirdGame:
         for pipe in self.pipes:
             pipe.draw()
 
+    def move(self):
+        self.bird.move()
+        for pipe in self.pipes:
+            pipe.move()
+
+    def lose_check(self):
+        if self.bird.y >= HEIGHT - 50 or self.bird.y <= 0:
+            return True
+
+        for pipe in self.pipes:
+            if self.bird.get_mask().overlap(pipe.get_top_mask(), pipe.get_top_offset(self.bird.x, self.bird.y)):
+                return True
+            if self.bird.get_mask().overlap(pipe.get_bottom_mask(), pipe.get_bottom_offset(self.bird.x, self.bird.y)):
+                return True
+
+        return False
+
+    def start_screen(self):
+        start_game = False
+
+        start_text = END_FONT.render("Press space to start", True, (255, 255, 255))
+        while not start_game:
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    sys.exit()
+                if i.type == pygame.KEYDOWN:
+                    if i.key == pygame.K_SPACE:
+                        start_game = not start_game
+
+            window.blit(self.bg_img, (0, 0))
+
+            window.blit(start_text, ((WIDTH - start_text.get_width()) / 2, 300))
+
+            pygame.display.flip()
+
     def play(self):
+        self.start_screen()
+
         playing = True
         while playing:
             pygame.time.delay(30)
             clock.tick(60)
             for i in pygame.event.get():
                 if i.type == pygame.QUIT:
-                    playing = False
+                    sys.exit()
                 if i.type == pygame.KEYDOWN:
                     if i.key == pygame.K_SPACE:
                         self.bird.jump()
 
-            self.bird.move()
-            for pipe in self.pipes:
-                pipe.move()
+            self.move()
             self.draw()
+
+            add_pipe = False
+            removed_pipe = None
+            for pipe in self.pipes:
+                if not pipe.passed_flag and self.bird.x > pipe.x + pipe.img.get_width() + 25:
+                    pipe.passed_flag = True
+                    removed_pipe = pipe
+                    add_pipe = True
+
+            if removed_pipe is not None:
+                self.pipes.remove(removed_pipe)
+                self.removed_pipes.append(removed_pipe)
+
+            if add_pipe:
+                self.pipes.append(Pipe(WIDTH))
+
+            playing = not self.lose_check()
+
             pygame.display.flip()
 
-        pygame.quit()
+        self.endgame()
+
+    def endgame(self):
+        restart_game = False
+
+        restart_text = END_FONT.render("Press space to restart", True, (255, 0, 255))
+        score = SCORE_FONT.render("Your score is {}".format(len(self.removed_pipes)), True, (255, 255, 255))
+        while not restart_game:
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    sys.exit()
+                if i.type == pygame.KEYDOWN:
+                    if i.key == pygame.K_SPACE:
+                        restart_game = True
+
+            window.blit(self.bg_img, (0, 0))
+
+            window.blit(score, ((WIDTH - score.get_width()) / 2, 300))
+            window.blit(restart_text, ((WIDTH - restart_text.get_width()) / 2, 500))
+
+            pygame.display.flip()
 
 
 def main():
-    game = FlappyBirdGame()
-    game.play()
+    while True:
+        game = FlappyBirdGame()
+        game.play()
 
 
 if __name__ == '__main__':
